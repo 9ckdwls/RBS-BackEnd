a281633790cf55c4cea5a90027a0a52e9697c93f
CJLEE\9ckdw <9ckdwls@naver.com>
2025-02-02 00:55:42 +0900

    test_user

--- rbs/src/main/java/com/example/rbs/entity/Box.java
+++ rbs/src/main/java/com/example/rbs/entity/Box.java
@@ -13,7 +13,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 @Setter
 public class Box {
 	@EmbeddedId
-    private BoxId boxId; // 복합 키 클래스
+    private BoxId boxId_tset_user; // 복합 키 클래스
 	
 	private String IPAddress;
 	
