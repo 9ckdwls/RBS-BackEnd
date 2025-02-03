ecf78aa4b4d6d15e9952d1869122ff58174debae
CJLEE\9ckdw <9ckdwls@naver.com>
2025-02-04 03:06:00 +0900

    admin8
    가입코드 조회 / 가입코드 변경

--- rbs/src/main/java/com/example/rbs/controller/CodeController.java
+++ rbs/src/main/java/com/example/rbs/controller/CodeController.java
@@ -1,5 +1,10 @@
 package com.example.rbs.controller;
 
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import com.example.rbs.service.CodeService;
 
@@ -10,6 +15,24 @@ public class CodeController {
 	
 	public CodeController(CodeService codeService) {
 		this.codeService = codeService;
+	}
+	
+	// 수거자 가입 코드 찾기
+	// 가입 코드는 오류 시 -1을 반환한다.
+	@GetMapping("admin/findCode")
+	public int findCode() {
+		return codeService.findCode();
+	}
+	
+	// 수거자 가입 코드 변경
+	@PatchMapping("admin/updateCode")
+	public String updateCode(@RequestBody Map<String, Integer> request) {
+		System.out.println("saljdlkas");
+		if(codeService.updateCode(request.get("code")) == 1) {
+			return "Success";
+		} else {
+			return "Fail";
+		}
 	}
 
 }
--- rbs/src/main/java/com/example/rbs/repository/CodeRepository.java
+++ rbs/src/main/java/com/example/rbs/repository/CodeRepository.java
@@ -1,10 +1,17 @@
 package com.example.rbs.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import com.example.rbs.entity.Code;
 
 @Repository
 public interface CodeRepository extends JpaRepository<Code, Integer> {
+	
+	// 수거자 가입 코드 찾기
+	@Query("select c from Code c")
+	public Optional<Code> findCode();
 
 }
