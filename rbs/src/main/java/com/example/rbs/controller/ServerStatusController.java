54381ebb888aa13eb43fdf034c17bef036dd5cee
CJLEE\9ckdw <9ckdwls@naver.com>
2025-02-05 04:01:15 +0900

    admin11
    DB / 앱 백 서버 / 사용자 앱 프론트 서버 / 수거자 앱 프론트 서버 상태 확인하기

---
+++ rbs/src/main/java/com/example/rbs/controller/ServerStatusController.java
@@ -0,0 +1,45 @@
+package com.example.rbs.controller;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+import javax.sql.DataSource;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.rbs.service.ServerStatusService;
+
+@RestController
+public class ServerStatusController {
+	
+	private ServerStatusService serverStatusService;
+	
+	public ServerStatusController(ServerStatusService serverStatusService) {
+		this.serverStatusService = serverStatusService;
+	}
+	
+	// 서버 상태 확인
+	// DB, 앱 백 서버, 사용자 앱 프론트 서버, 수거자 앱 프론트 서버 상태
+	@GetMapping("admin/serverStatus")
+	public Map<String, String> serverStatus() {
+		return serverStatusService.serverStatus();
+	}
+	
+	// 사용자 앱 프론트 서버 상태 업데이트
+	@GetMapping("admin/userFrontServer")
+	public void userFrontServer() {
+		serverStatusService.updateUserHeartbeat();
+	}
+	
+	// 수거자 앱 프론트 서버 상태 업데이트
+	@GetMapping("admin/employeeFrontServer")
+	public void employeeFrontServer() {
+		serverStatusService.updateEmployeeHeartbeat();
+	}
+	
+	
+}
---
+++ rbs/src/main/java/com/example/rbs/service/ServerStatusService.java
@@ -0,0 +1,87 @@
+package com.example.rbs.service;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.time.Duration;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import javax.sql.DataSource;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Service
+public class ServerStatusService {
+
+	private DataSource dataSource;
+	private WebClient.Builder webClientBuilder;
+
+	// 멀티스레드 환경에서 동시 접근을 지원하는 해시맵
+	private final ConcurrentHashMap<String, Long> lastHeartbeatUser = new ConcurrentHashMap<>();
+	private final ConcurrentHashMap<String, Long> lastHeartbeatEmployee = new ConcurrentHashMap<>();
+
+	public ServerStatusService(DataSource dataSource, WebClient.Builder webClientBuilder) {
+		this.dataSource = dataSource;
+		this.webClientBuilder = webClientBuilder;
+	}
+
+	// 서버 상태 확인
+	// DB, 앱 백 서버, 사용자 앱 프론트 서버, 수거자 앱 프론트 서버 상태
+	public Map<String, String> serverStatus() {
+
+		Map<String, String> status = new HashMap<>();
+
+		// 사용자 앱 프론트 서버 상태 확인
+		long lastUserHeartbeat = lastHeartbeatUser.getOrDefault("reactNative", 0L);
+		if (System.currentTimeMillis() - lastUserHeartbeat < 300000) {
+			status.put("userApp", "UP");
+		} else {
+			status.put("userApp", "DOWN");
+		}
+
+		// 수거자 앱 프론트 서버 상태 확인
+		long lastEmployeeHeartbeat = lastHeartbeatEmployee.getOrDefault("reactNative", 0L);
+		if (System.currentTimeMillis() - lastEmployeeHeartbeat < 300000) {
+			status.put("employeeApp", "UP");
+		} else {
+			status.put("employeeApp", "DOWN");
+		}
+
+		// DB 상태 확인
+		try (Connection conn = dataSource.getConnection()) {
+			if (!conn.isClosed()) {
+				status.put("database", "UP");
+			} else {
+				status.put("database", "DOWN");
+			}
+		} catch (SQLException e) {
+			status.put("database", "DOWN");
+		}
+
+		// 앱 백 서버 상태 확인
+		WebClient webClient = webClientBuilder.build();
+
+		try {
+			// 응답을 동기적으로 받아서 처리
+			String response = webClient.get().uri("sd").retrieve().bodyToMono(String.class)
+					.timeout(Duration.ofSeconds(5)).block(); // 응답이 올 때까지 대기
+
+			status.put("appServer", "UP"); // 정상 응답이면 UP
+		} catch (Exception e) {
+			status.put("appServer", "DOWN"); // 오류 발생 시 DOWN
+		}
+
+		return status;
+	}
+
+	// 사용자 앱 프론트 서버 상태 업데이트
+	public void updateUserHeartbeat() {
+		lastHeartbeatUser.put("reactNative", System.currentTimeMillis());
+	}
+
+	// 수거자 앱 프론트 서버 상태 업데이트
+	public void updateEmployeeHeartbeat() {
+		lastHeartbeatEmployee.put("reactNative", System.currentTimeMillis());
+	}
+
+}
