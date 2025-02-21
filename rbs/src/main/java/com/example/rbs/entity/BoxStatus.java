981db98b8bcd9d730d69e833c3a7ab4ce84073f3
CJLEE\9ckdw <9ckdwls@naver.com>
2025-02-21 14:31:29 +0900

    master15
    AlarmType와 BoxStatus 속성 추가

--- rbs/src/main/java/com/example/rbs/entity/BoxStatus.java
+++ rbs/src/main/java/com/example/rbs/entity/BoxStatus.java
@@ -5,10 +5,13 @@ public enum BoxStatus {
     AVAILABLE("사용가능"),
     BLOCKED("차단"),
     INSTALL_REQUEST("설치 요청"),
+    INSTALL_IN_PROGRESS("설치 진행"),
     INSTALL_COMPLETED("설치 완료"),
     INSTALL_CONFIRMED("설치 확정"),
     REMOVE_REQUEST("제거 요청"),
+    REMOVE_IN_PROGRESS("제거 진행"),
     REMOVE_COMPLETED("제거 완료"),
+    REMOVE_CONFIRMED("제거 확정"),
     COLLECTION_NEEDED("수거 필요"),
     COLLECTION_RECOMMENDED("수거 권장");
 
