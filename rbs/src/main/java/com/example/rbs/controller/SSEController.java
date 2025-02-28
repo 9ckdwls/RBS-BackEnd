2db3ac5e420e82ed43f988b5e51b01ffddff2de3
CJLEE\9ckdw <9ckdwls@naver.com>
2025-03-01 01:29:08 +0900

    admin22
    SSE 최초 테스트

---
+++ rbs/src/main/java/com/example/rbs/controller/SSEController.java
@@ -0,0 +1,27 @@
+package com.example.rbs.controller;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+import com.example.rbs.service.SSEService;
+
+@RestController
+public class SSEController {
+	
+	private final SSEService sseService;
+	
+	public SSEController(SSEService sseService) {
+		this.sseService = sseService;
+	}
+	
+	@GetMapping()
+	public SseEmitter subscribe() {
+		return sseService.subscribe();
+	}
+	
+	
+}
---
+++ rbs/src/main/java/com/example/rbs/service/SSEService.java
@@ -0,0 +1,48 @@
+package com.example.rbs.service;
+
+import java.io.IOException;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+@Service
+public class SSEService {
+	
+	private final UserService userService;
+	
+	public SSEService(UserService userService) {
+		this.userService = userService;
+	}
+	
+	private final Map<String, SseEmitter> emitters = new ConcurrentHashMap<>();
+
+	public SseEmitter subscribe() {
+		
+		String userId = userService.getUserId();
+		
+		if (emitters.containsKey(userId)) {
+	        emitters.get(userId).complete();
+	        emitters.remove(userId);
+	    }
+		
+		SseEmitter emitter = new SseEmitter(0L);
+        emitters.put(userId, emitter);
+        
+        emitter.onCompletion(() -> emitters.remove(userId));		
+		return emitter;
+	}
+	
+	public void sendAlarmToUser(String userId, String message) {
+        SseEmitter emitter = emitters.get(userId);
+        if (emitter != null) {
+            try {
+                emitter.send(SseEmitter.event().name("alarm").data(message));
+            } catch (IOException e) {
+                emitters.remove(userId);
+            }
+        }
+    }
+
+}
