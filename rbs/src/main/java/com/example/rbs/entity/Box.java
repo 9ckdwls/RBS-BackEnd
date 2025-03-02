7a0b7e0583fcdc6f9f20901dc6262fd0e5dd12ed
CJLEE\9ckdw <9ckdwls@naver.com>
2025-03-02 09:31:19 +0900

    master16
    Box 및 Alarm 스키마 변경

--- rbs/src/main/java/com/example/rbs/entity/Box.java
+++ rbs/src/main/java/com/example/rbs/entity/Box.java
@@ -29,16 +29,39 @@ public class Box {
 	
 	private Point location;
 	
+	@Enumerated(EnumType.STRING)
+	private InstallStatus installStatus;
+	
 	private int volume;
 	
-	private int fire;
+	@Enumerated(EnumType.STRING)
+	private FireStatus fireStatus;
 	
 	@Enumerated(EnumType.STRING)
-	private BoxStatus used;
+	private UsageStatus usageStatus;
 	
 	//반환되는 JSON 값을 문자열로 바꾸기
 	@JsonProperty("location")
     public String getLocationAsText() {
         return location != null ? location.toText() : null;
+    }
+	
+	public enum InstallStatus {
+        INSTALL_REQUEST, 
+        INSTALL_IN_PROGRESS, 
+        INSTALL_COMPLETED, 
+        INSTALL_CONFIRMED,
+        REMOVE_REQUEST, 
+        REMOVE_IN_PROGRESS, 
+        REMOVE_COMPLETED, 
+        REMOVE_CONFIRMED
+    }
+
+    public enum UsageStatus {
+        AVAILABLE, BLOCKED, USED
+    }
+    
+    public enum FireStatus {
+    	FIRE, UNFIRE
     }
 }
