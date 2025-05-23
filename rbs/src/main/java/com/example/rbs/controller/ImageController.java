cddc0374fe8ea824b5b0a215fdeb811f69209fe2
CJLEE\9ckdw <9ckdwls@naver.com>
2025-05-23 19:22:39 +0900

    admin57
    분리로그 사진 JSON 오류로 3가지 API로 나눔

--- rbs/src/main/java/com/example/rbs/controller/ImageController.java
+++ rbs/src/main/java/com/example/rbs/controller/ImageController.java
@@ -11,37 +11,47 @@ import com.example.rbs.service.ImageService;
 
 @RestController
 public class ImageController {
-	
+
 	private final ImageService imageService;
-	
+
 	public ImageController(ImageService imageService) {
 		this.imageService = imageService;
 	}
-	
+
 	// 수거함 이미지
 	@GetMapping(value = "admin/boxImage/{boxId}", produces = MediaType.IMAGE_JPEG_VALUE)
 	public byte[] getBoxImage(@PathVariable(value = "boxId") int boxId) {
 		return imageService.getBoxImage(boxId);
 	}
-	
+
 	// 화재처리 이미지
 	@GetMapping(value = "admin/fireImage/{alarmId}", produces = MediaType.IMAGE_JPEG_VALUE)
 	public byte[] getFireImage(@PathVariable(value = "alarmId") int alarmId) {
 		return imageService.getFireImage(alarmId);
 	}
-	
+
 	// 수거 이미지
 	@GetMapping(value = "admin/collectionImage/{boxLogId}", produces = MediaType.IMAGE_JPEG_VALUE)
 	public byte[] getCollectionImage(@PathVariable(value = "boxLogId") int boxLogId) {
 		return imageService.getCollectionImage(boxLogId);
 	}
-	
+
 	// 분리 이미지
-	// map에 battery, discharged, notDischarged를 key로 각 배터리, 방전된 폐전지, 방전되지 않은 폐전지
-	@GetMapping(value = "admin/itemsImage/{boxLogId}", produces = MediaType.IMAGE_JPEG_VALUE)
-	public Map<String, byte[]> getItemsImage(@PathVariable(value = "boxLogId") int boxLogId) {
-		return imageService.getItemsImage(boxLogId);
+	@GetMapping(value = "admin/getBatteryImage/{boxLogId}", produces = MediaType.IMAGE_JPEG_VALUE)
+	public byte[] getBatteryImage(@PathVariable(value = "boxLogId") int boxLogId) {
+		return imageService.getBatteryImage(boxLogId);
 	}
-	
+
+	// 분리 이미지
+	@GetMapping(value = "admin/getDischargedImage/{boxLogId}", produces = MediaType.IMAGE_JPEG_VALUE)
+	public byte[] getDischargedImage(@PathVariable(value = "boxLogId") int boxLogId) {
+		return imageService.getDischargedImage(boxLogId);
+	}
+
+	// 분리 이미지
+	@GetMapping(value = "admin/getUndischargedImage/{boxLogId}", produces = MediaType.IMAGE_JPEG_VALUE)
+	public byte[] getUndischargedImage(@PathVariable(value = "boxLogId") int boxLogId) {
+		return imageService.getUndischargedImage(boxLogId);
+	}
 
 }
--- rbs/src/main/java/com/example/rbs/service/ImageService.java
+++ rbs/src/main/java/com/example/rbs/service/ImageService.java
@@ -14,62 +14,76 @@ import com.example.rbs.entity.BoxLog;
 
 @Service
 public class ImageService {
-	
+
 	private final BoxService boxService;
 	private final AlarmService alarmService;
 	private final BoxLogService boxLogService;
-	
+
 	public ImageService(BoxService boxService, AlarmService alarmService, BoxLogService boxLogService) {
 		this.boxService = boxService;
 		this.alarmService = alarmService;
 		this.boxLogService = boxLogService;
 	}
-	
+
 	// 수거함 이미지
 	public byte[] getBoxImage(int boxId) {
 		return getImage(boxService.findById(boxId).getFile());
 	}
-	
+
 	// 화재처리 이미지
 	public byte[] getFireImage(int alarmId) {
 		return getImage(alarmService.findById(alarmId).getFile());
 	}
-	
+
 	// 수거 이미지
 	public byte[] getCollectionImage(int boxLogId) {
 		return getImage(boxLogService.findById(boxLogId).getCollection_file());
 	}
-	
-	// 분리 이미지
-	public Map<String, byte[]> getItemsImage(int boxLogId) {
-		Map<String, byte[]> images = new HashMap<>();
+
+	// 분리 배터리 이미지
+	public byte[] getBatteryImage(int boxLogId) {
 		BoxLog log = boxLogService.findById(boxLogId);
 		if (log.getFile_battery() != null) {
-            images.put("battery", getImage(log.getFile_battery()));
-        }
-        if (log.getFile_discharged() != null) {
-            images.put("discharged", getImage(log.getFile_discharged()));
-        }
-        if (log.getFile_not_discharged() != null) {
-            images.put("notDischarged", getImage(log.getFile_not_discharged()));
-        }
-		return images;
+			return getImage(log.getFile_battery());
+		} else {
+			return null;
+		}
+	}
+
+	// 분리 방전된 폐전지 이미지
+	public byte[] getDischargedImage(int boxLogId) {
+		BoxLog log = boxLogService.findById(boxLogId);
+		if (log.getFile_discharged() != null) {
+			return getImage(log.getFile_discharged());
+		} else {
+			return null;
+		}
+	}
+
+	// 분리 방전되지 않은 폐전지 이미지
+	public byte[] getUndischargedImage(int boxLogId) {
+		BoxLog log = boxLogService.findById(boxLogId);
+		if (log.getFile_not_discharged() != null) {
+			return getImage(log.getFile_not_discharged());
+		} else {
+			return null;
+		}
 	}
 
 	// 사진 파일 얻기
 	public byte[] getImage(String file) {
-		
+
 		try {
 			Path path = Paths.get("C:/uploads/images/" + file);
 			if (!Files.exists(path)) {
-                return null;
-            }
+				return null;
+			}
 			byte[] data = Files.readAllBytes(path);
-			
+
 			return data;
 		} catch (IOException e) {
 			return null;
 		}
 	}
-	
+
 }
