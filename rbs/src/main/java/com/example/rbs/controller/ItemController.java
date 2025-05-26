7eb2909fc8fc74ecfcc2cb58c4512e46ebacf42b
CJLEE\9ckdw <9ckdwls@naver.com>
2025-05-27 01:58:45 +0900

    admin63
    상품 전부가져오기

--- rbs/src/main/java/com/example/rbs/controller/ItemController.java
+++ rbs/src/main/java/com/example/rbs/controller/ItemController.java
@@ -1,14 +1,24 @@
 package com.example.rbs.controller;
 
-import org.springframework.web.bind.annotation.RestController;
-import com.example.rbs.service.ItemService;
+import java.util.List;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.rbs.entity.Item;
+import com.example.rbs.service.ItemService;
 
 @RestController
 public class ItemController {
 	private final ItemService itemService;
-	
+
 	public ItemController(ItemService itemService) {
 		this.itemService = itemService;
+	}
+
+	// 상품 전부 가져오기
+	@GetMapping("admin/getItems")
+	public List<Item> getItems() {
+		return itemService.getItems();
 	}
 }
--- rbs/src/main/java/com/example/rbs/service/ItemService.java
+++ rbs/src/main/java/com/example/rbs/service/ItemService.java
@@ -1,15 +1,25 @@
 package com.example.rbs.service;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.stereotype.Service;
+
+import com.example.rbs.entity.Item;
 import com.example.rbs.repository.ItemRepository;
 
 @Service
 public class ItemService {
-	
+
 	private final ItemRepository itemRepository;
-	
+
 	public ItemService(ItemRepository itemRepository) {
 		this.itemRepository = itemRepository;
 	}
-	
+
+	// 상품 전부 가져오기
+	public List<Item> getItems() {
+		return itemRepository.findAll();
+	}
+
 }
